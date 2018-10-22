6b40d8933390d033ff8472862f958bfcbf47e6fd
Julius Ferianc <julius@ferianc.com>
2018-10-22 18:08:39 +0200

    dictionary kickoff, class homework assignment

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-print("Toto bude za chvili nejgenialnejsi slovnik")
-print("Udela jej Jakub")
-print("vytvori TomasNepala")
-print("udela jej Anezka")
-print("nitram ji pomuze")
-print("+ Marian")
+print("Vitej ve slovniku (CZ-EN)")
+language_from = input("Z jakeho jazyka? (CZ nebo EN)")
+
+if not language_from in ("CZ", "EN"):
+	print("neni podporovany jazyk")
+	exit(101)
+
+words_cz = ["pivo", "salam", "pocitac"]
+words_en = ["beer", "sausage", "computer"]
+
+word = input("Zadej slovo na preklad")
+
+match = False
+pozice = 0
+if language_from == "CZ":
+	for slovo in words_cz:
+		if slovo == word:
+			match = pozice
+		pozice = pozice + 1
+
+if match == False:
+	print("Toto slovo nemam v slovniku")
+	exit(102)
+
+print(words_en[match])
+
+# print("Nasel jsem slovo na pozici " + str(match))
