97c807dbef11ddfa27678018b3ef8e5fd440c7bd
Julius Ferianc <julius@ferianc.com>
2018-10-29 17:52:37 +0100

    refactoring and fixes in mariann's branch

--- main.py
+++ main.py
@@ -1,40 +1,47 @@
-print("Vitej ve slovniku (CZ-EN)!")
-again = True
+def translate_word_from_language(word, language_from):
 
-while again is True:
     trans_index = 0
     match = False
     pozice = 0
 
-    language_from = input("Z jakeho jazyka? (CZ nebo EN): ")
-
-    if not language_from in ("CZ", "EN", "cz", "en"):
-        print("!!!NENI PODPOROVANY JAZYK!!!")
-        exit(101)
-
     words_cz = ["pivo", "salam", "pocitac"]
     words_en = ["beer", "sausage", "computer"]
 
+    if language_from == "CZ":
+        words_from = words_cz
+        words_to = words_en
+    elif language_from == "EN":
+        words_from = words_en
+        words_to = words_cz
+
+    for slovo in words_from:
+        if slovo == word:
+            match = True
+            trans_index = pozice
+            break
+        pozice = pozice + 1
+    if match == True:
+        print(str(word) + " = " + words_to[trans_index])
+    else:
+        print("Toto slovo nemam v databazi!")
+        # exit(102)
+
+
+print("Vitej ve slovniku (CZ-EN)!")
+again = True
+
+while again is True:
+
+    language_from = input("Z jakeho jazyka? (CZ nebo EN): ")
+    language_from = language_from.upper()
+
+    if not language_from in ("CZ", "EN"):
+        print("!!!NENI PODPOROVANY JAZYK!!!")
+        exit(101)
+
     word = input("Zadej slovo na preklad: ")
 
-    if language_from in ("CZ", "cz"):
-        for slovo in words_cz:
-            if slovo == word:
-                match = True
-                trans_index = pozice
-            pozice = pozice + 1
-        print(str(word) + " = " + words_en[trans_index])
-    if language_from in ("EN", "en"):
-        for slovo in words_en:
-            if slovo == word:
-                match = True
-                trans_index = pozice
-            pozice = pozice + 1
-        print(str(word) + " = " + words_cz[trans_index])
-
-    if match is False:
-        print("Toto slovo nemam v databazi!")
-        exit(102)
+    translate_word_from_language(word, language_from)
 
     again = input("Chces prelozit jeste neco? ")
 
@@ -42,3 +49,5 @@ while again is True:
         again = True
     else:
         again = False
+
+
