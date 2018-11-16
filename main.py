750ea8787df35c78a9681771143ea5f185442070
tomas <tomas.nepala@student.englishcollege.cz>
2018-11-16 09:09:59 +0100

    Dodelal jsem text to image

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from PIL import Image, ImageDraw
 import string
 import random
 
-def id_generator(size=6, chars=string.ascii_lowercase + string.digits):
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
 	return ''.join(random.choice(chars) for _ in range(size))
 
 
@@ -10,4 +11,11 @@ for x in range(100):
 	print(str(x) + '. ' + S)
 
 S = id_generator(5)
-print(S)
+
+
+img = Image.new('RGB', (100, 30), color=(73, 109, 137))
+
+d = ImageDraw.Draw(img)
+d.text((10, 10), S, fill=(255, 255, 0))
+
+img.save('pil_text.png')
