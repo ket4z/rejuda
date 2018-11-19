be8a9c86804473f8f8d8634781406c6f5e8e29a7
Julius Ferianc <julius@ferianc.com>
2018-11-19 17:16:59 +0100

    stash small change from homework

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
 	return ''.join(random.choice(chars) for _ in range(size))
 
 
-for x in range(100):
-	S = id_generator(5)
-	print(str(x) + '. ' + S)
+# for x in range(100):
+# 	S = id_generator(5)
+# 	print(str(x) + '. ' + S)
 
 S = id_generator(5)
-
+print(S)
 
 img = Image.new('RGB', (100, 30), color=(73, 109, 137))
 
