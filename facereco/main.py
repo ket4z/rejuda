290b5a724bed11ff8b4f82ec0ed4deba7d1149aa
Julius Ferianc <julius@ferianc.com>
2018-11-26 17:40:43 +0100

    tkinter file open dialog

--- facereco/main.py
+++ facereco/main.py
@@ -1,8 +1,21 @@
-from PIL import Image, ImageDraw
+from PIL import ImageDraw
+import PIL.Image
 import face_recognition
+from tkinter import filedialog
+from tkinter import *
+
+root = Tk()
+# TODO: HW1 zacit v home adresari uzivatele ktery tento kod spousti (initialdir)
+
+root.filename = filedialog.askopenfilename(
+	initialdir = "/home/julius/Downloads",
+	title = "Select file",
+	filetypes = (("jpeg files","*.jpg"), ("png files","*.png")))
+print ("working on file > " + root.filename)
 
 # Load the jpg file into a numpy array
-image = face_recognition.load_image_file("test2.jpg")
+image = face_recognition.load_image_file(root.filename)
+# image = face_recognition.load_image_file("test2.jpg")
 # image = face_recognition.load_image_file("koule.jpg")
 
 xichtove_lokace = face_recognition.face_locations(image)
@@ -11,7 +24,7 @@ print("I found {} face(s) in this photograph.".format(len(xichtove_lokace)))
 
 # Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
 # See http://pillow.readthedocs.io/ for more about PIL/Pillow
-pil_image = Image.fromarray(image)
+pil_image = PIL.Image.fromarray(image)
 # Create a Pillow ImageDraw Draw instance to draw with
 draw = ImageDraw.Draw(pil_image)
 
