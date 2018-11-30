752bbd4ec6c9b0f0d6a43db1f5c4b0968ea67a98
Marian <lol26pp@gmail.com>
2018-11-30 16:38:16 +0100

    1. loading and saving into /home initialdir of any user 2. added window with find file and save file buttons and two default images Problem: cant load founded image and image after face reco

--- facereco/main.py
+++ facereco/main.py
@@ -3,63 +3,103 @@ import PIL.Image
 import face_recognition
 from tkinter import filedialog
 from tkinter import *
+from os import path
 
 root = Tk()
-# TODO: HW1 zacit v home adresari uzivatele ktery tento kod spousti (initialdir)
-root.filename = filedialog.askopenfilename(
-	initialdir = "/home/julius/Downloads",
-	title = "Select file",
-	filetypes = (("jpeg files","*.jpg"), ("png files","*.png")))
-print ("working on file > " + root.filename)
+root.title("Face recognition")
+root.geometry("300x150")
+home = path.expanduser('~')
+imageBefore = PhotoImage(file="default-image.jpg")
+imageAfter = PhotoImage(file="default-image.jpg")
 
-# Load the jpg file into a numpy array
-image = face_recognition.load_image_file(root.filename)
-# image = face_recognition.load_image_file("test2.jpg")
-# image = face_recognition.load_image_file("koule.jpg")
 
-xichtove_lokace = face_recognition.face_locations(image)
+class MainWindow:
 
-print("I found {} face(s) in this photograph.".format(len(xichtove_lokace)))
+    def __init__(self, master):
 
-# Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
-# See http://pillow.readthedocs.io/ for more about PIL/Pillow
-pil_image = PIL.Image.fromarray(image)
-# Create a Pillow ImageDraw Draw instance to draw with
-draw = ImageDraw.Draw(pil_image)
+        self.imageBefore = imageBefore
+        self.imageAfter = imageAfter
 
-for xicht in xichtove_lokace:
+        frame = Frame(master)
+        frame.pack()
 
-    # Print the location of each face in this image
-    # top, right, bottom, left = xicht
-    top = xicht[0]
-    right = xicht[1]
-    bottom = xicht[2]
-    left = xicht[3]
+        self.lblHeader = Label(frame, text="Recognize face(s) from your own photo!")
+        self.lblHeader.pack()
 
-    print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+        self.btnFindFile = Button(frame, text="Find file", command=self.findFile)
+        self.btnFindFile.pack()
 
-    # You can access the actual face itself like this:
-    # face_image = image[top:bottom, left:right]
-    # pil_image = Image.fromarray(face_image)
-    # pil_image.show()
+        self.btnFindFile = Button(frame, text="Save file", command=self.saveFile)
+        self.btnFindFile.pack()
 
-    # modry boxik
-    draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
-    # cerveny kruh
-    draw.ellipse(((left, top), (right, bottom)), None, outline=(255, 0, 0))
+        self.imgBefore = Label(frame, image=self.imageBefore)
+        self.imgBefore.pack()
 
-# free the drawing library from memory
-del draw
+        self.imgAfter = Label(frame, image=self.imageAfter)
+        self.imgAfter.pack()
 
-# display the resulting image
-pil_image.show()
+    def findFile(self):
+        root.filename = filedialog.askopenfilename(
+            initialdir=home,
+            title="Select file",
+            filetypes=(("jpeg files", "*.jpg"), ("png files", "*.png")))
+        print("working on file > " + root.filename)
 
-# save a copy of the new image to disk?
-# TODO: HW1 defaultne v home adresari uzivatele ktery tento kod spousti (initialdir), stejne jako nahore u open
-root.filename = filedialog.asksaveasfilename(
-	initialdir = "/home/julius/Downloads",
-	title = "Select file",
-	filetypes = (("jpeg files","*.jpg"), ("png files","*.png")))
-print ("saving on file > " + root.filename)
+        # Load the jpg file into a numpy array
+        image = face_recognition.load_image_file(root.filename)
+        # image = face_recognition.load_image_file("test2.jpg")
+        # image = face_recognition.load_image_file("koule.jpg")
 
-pil_image.save(root.filename)
+        xichtove_lokace = face_recognition.face_locations(image)
+
+        print("I found {} face(s) in this photograph.".format(len(xichtove_lokace)))
+
+        # Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
+        # See http://pillow.readthedocs.io/ for more about PIL/Pillow
+        pil_image = PIL.Image.fromarray(image)
+        # Create a Pillow ImageDraw Draw instance to draw with
+        draw = ImageDraw.Draw(pil_image)
+
+        for xicht in xichtove_lokace:
+            # Print the location of each face in this image
+            # top, right, bottom, left = xicht
+            top = xicht[0]
+            right = xicht[1]
+            bottom = xicht[2]
+            left = xicht[3]
+
+            print(
+                "A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom,
+                                                                                                      right))
+
+            # You can access the actual face itself like this:
+            # face_image = image[top:bottom, left:right]
+            # pil_image = Image.fromarray(face_image)
+            # pil_image.show()
+
+            # modry boxik
+            draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
+            # cerveny kruh
+            draw.ellipse(((left, top), (right, bottom)), None, outline=(255, 0, 0))
+
+        # free the drawing library from memory
+        del draw
+
+        # display the resulting image
+        self.imageAfter = pil_image
+        self.imageBefore = image
+        root.update()
+
+    def saveFile(self):
+        # save a copy of the new image to disk?
+        root.filename = filedialog.asksaveasfilename(
+            initialdir=home,
+            title="Select file",
+            filetypes=(("jpeg files", "*.jpg"), ("png files", "*.png")))
+        print("saving on file > " + root.filename)
+
+        self.imageAfter.save(root.filename)
+
+
+mw = MainWindow(root)
+root.mainloop()
